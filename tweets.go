4805fda2d519092e43436b59a0a73ff0fb865ba9
银河 <8054162+yinhe418@user.noreply.gitee.com>
2024-12-12 16:14:30 +0800

    add new func

--- tweets.go
+++ tweets.go
@@ -212,3 +212,112 @@ func (s *Scraper) GetTweetDetails(id string) ([]*Tweet, error) {
 	}
 	return nil, fmt.Errorf("tweet with ID %s not found", id)
 }
+
+func (s *Scraper) GetUserTweetsAndReplies(userID string, maxTweetsNbr int, cursor string) ([]*Tweet, string, error) {
+	req, err := s.newRequest("GET", "https://twitter.com/i/api/graphql/RIWc55YCNyUJ-U3HHGYkdg/UserTweetsAndReplies")
+	if err != nil {
+		return nil, "", err
+	}
+	variables := map[string]interface{}{
+		"userId":                                 userID,
+		"count":                                  maxTweetsNbr,
+		"withSafetyModeUserFields":               true,
+		"includePromotedContent":                 true,
+		"withQuickPromoteEligibilityTweetFields": true,
+		"withVoice":                              true,
+		"withV2Timeline":                         true,
+		"withDownvotePerspective":                false,
+		"withBirdwatchNotes":                     true,
+		"withCommunity":                          true,
+		"withSuperFollowsUserFields":             true,
+		"withReactionsMetadata":                  false,
+		"withReactionsPerspective":               false,
+		"withSuperFollowsTweetFields":            true,
+		"isMetatagsQuery":                        false,
+		"withReplays":                            true,
+		"withClientEventToken":                   false,
+		"withAttachments":                        true,
+		"withConversationQueryHighlights":        true,
+		"withMessageQueryHighlights":             true,
+		"withMessages":                           true,
+	}
+
+	features := map[string]interface{}{
+		//"rweb_lists_timeline_redesign_enabled":                                    true,
+		//"responsive_web_graphql_exclude_directive_enabled":                        true,
+		//"verified_phone_label_enabled":                                            false,
+		//"creator_subscriptions_tweet_preview_api_enabled":                         true,
+		//"responsive_web_graphql_timeline_navigation_enabled":                      true,
+		//"responsive_web_graphql_skip_user_profile_image_extensions_enabled":       false,
+		//"tweetypie_unmention_optimization_enabled":                                true,
+		//"responsive_web_edit_tweet_api_enabled":                                   true,
+		//"graphql_is_translatable_rweb_tweet_is_translatable_enabled":              true,
+		//"view_counts_everywhere_api_enabled":                                      true,
+		//"longform_notetweets_consumption_enabled":                                 true,
+		//"tweet_awards_web_tipping_enabled":                                        false,
+		//"freedom_of_speech_not_reach_fetch_enabled":                               true,
+		//"standardized_nudges_misinfo":                                             true,
+		//"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": false,
+		//"longform_notetweets_rich_text_read_enabled":                              true,
+		//"longform_notetweets_inline_media_enabled":                                true,
+		//"responsive_web_enhance_cards_enabled":                                    false,
+		"c9s_tweet_anatomy_moderator_badge_enabled":    true,
+		"responsive_web_home_pinned_timelines_enabled": true,
+
+		"blue_business_profile_image_shape_enabled":                               true,
+		"creator_subscriptions_tweet_preview_api_enabled":                         true,
+		"freedom_of_speech_not_reach_fetch_enabled":                               true,
+		"graphql_is_translatable_rweb_tweet_is_translatable_enabled":              true,
+		"graphql_timeline_v2_bookmark_timeline":                                   true,
+		"hidden_profile_likes_enabled":                                            true,
+		"highlights_tweets_tab_ui_enabled":                                        true,
+		"interactive_text_enabled":                                                true,
+		"longform_notetweets_consumption_enabled":                                 true,
+		"longform_notetweets_inline_media_enabled":                                true,
+		"longform_notetweets_rich_text_read_enabled":                              true,
+		"longform_notetweets_richtext_consumption_enabled":                        true,
+		"profile_foundations_tweet_stats_enabled":                                 true,
+		"profile_foundations_tweet_stats_tweet_frequency":                         true,
+		"responsive_web_birdwatch_note_limit_enabled":                             true,
+		"responsive_web_edit_tweet_api_enabled":                                   true,
+		"responsive_web_enhance_cards_enabled":                                    false,
+		"responsive_web_graphql_exclude_directive_enabled":                        true,
+		"responsive_web_graphql_skip_user_profile_image_extensions_enabled":       false,
+		"responsive_web_graphql_timeline_navigation_enabled":                      true,
+		"responsive_web_media_download_video_enabled":                             false,
+		"responsive_web_text_conversations_enabled":                               false,
+		"responsive_web_twitter_article_data_v2_enabled":                          true,
+		"responsive_web_twitter_article_tweet_consumption_enabled":                false,
+		"responsive_web_twitter_blue_verified_badge_is_enabled":                   true,
+		"rweb_lists_timeline_redesign_enabled":                                    true,
+		"spaces_2022_h2_clipping":                                                 true,
+		"spaces_2022_h2_spaces_communities":                                       true,
+		"standardized_nudges_misinfo":                                             true,
+		"subscriptions_verification_info_verified_since_enabled":                  true,
+		"tweet_awards_web_tipping_enabled":                                        false,
+		"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": true,
+		"tweetypie_unmention_optimization_enabled":                                true,
+		"verified_phone_label_enabled":                                            false,
+		"vibe_api_enabled":                                                        true,
+		"view_counts_everywhere_api_enabled":                                      true,
+	}
+
+	if cursor != "" {
+		variables["cursor"] = cursor
+	}
+
+	query := url.Values{}
+	query.Set("variables", mapToJSONString(variables))
+	query.Set("features", mapToJSONString(features))
+	req.URL.RawQuery = query.Encode()
+
+	var timeline timelineV2
+	err = s.RequestAPI(req, &timeline)
+	if err != nil {
+		return nil, "", err
+	}
+
+	tweets, nextCursor := timeline.parseTweets()
+	return tweets, nextCursor, nil
+
+}
